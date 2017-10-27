9d3211756bbd9bff871c1de70dcbd7fd96a0757e
michal.hajek <mikrop@centrum.cz>
2017-10-27 21:59:47 +0200

    Heroku konfigurace + deploy

--- src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
+++ src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
@@ -2,6 +2,7 @@ package cz.mikropsoft.mhdwidget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.ProjectionFactory;
@@ -9,10 +10,10 @@ import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
 @Configuration
 @SpringBootApplication
-public class Application {
+public class MhdWidgetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(MhdWidgetApplication.class, args);
     }
 
     @Bean
