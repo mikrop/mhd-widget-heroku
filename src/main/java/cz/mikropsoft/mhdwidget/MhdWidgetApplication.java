17fb423c7b8b69cc8d33232ecc895b6299a4ae35
michal.hajek <mikrop@centrum.cz>
2017-10-28 15:13:26 +0200

    Zapnutí časovače

--- src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
+++ src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
@@ -2,13 +2,14 @@ package cz.mikropsoft.mhdwidget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 @SpringBootApplication
 public class MhdWidgetApplication {
 
