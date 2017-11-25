1bb4544345e5850c5b472c8c585e97af61675fbd
michal.hajek <mikrop@centrum.cz>
2017-11-25 17:57:51 +0100

    Optimalizace importů

--- src/test/java/cz/mikropsoft/mhdwidget/ApplicationTests.java
+++ src/test/java/cz/mikropsoft/mhdwidget/ApplicationTests.java
@@ -1,18 +1,12 @@
 package cz.mikropsoft.mhdwidget;
 
-import cz.mikropsoft.mhdwidget.MhdExporter;
-import cz.mikropsoft.mhdwidget.MhdWidgetApplication;
 import cz.mikropsoft.mhdwidget.model.Linka;
 import cz.mikropsoft.mhdwidget.model.Zastavka;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
