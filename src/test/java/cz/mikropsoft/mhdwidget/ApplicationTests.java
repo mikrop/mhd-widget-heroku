1928cc69f129e0ea34c5169cd08d2c425701ac01
michal.hajek <mikrop@centrum.cz>
2017-11-26 13:12:30 +0100

    Databázové testy

--- src/test/java/cz/mikropsoft/mhdwidget/ApplicationTests.java
+++ src/test/java/cz/mikropsoft/mhdwidget/ApplicationTests.java
@@ -8,10 +8,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MhdWidgetApplication.class)
@@ -22,10 +28,14 @@ public class ApplicationTests {
 
     @Test
     public void testExport() throws IOException, URISyntaxException {
-        Iterable<Linka> linky = exporter.linkySave();
-        Linka linka = linky.iterator().next();
-        Iterable<Zastavka> zastavky = exporter.zastavkyUpdate(Collections.singletonList(linka));
-        Assert.assertNotNull(zastavky);
+
+        Linka next = exporter.linkySave().iterator().next();
+        Collection<Linka> linky = Collections.singletonList(next);
+        Spliterator<Zastavka> spliterator = exporter.zastavkyUpdate(linky).spliterator();
+        List<Zastavka> zastavky = StreamSupport.stream(spliterator, false)
+                .collect(Collectors.toList());
+        Assert.assertFalse(CollectionUtils.isEmpty(zastavky));
+
     }
 
 }
