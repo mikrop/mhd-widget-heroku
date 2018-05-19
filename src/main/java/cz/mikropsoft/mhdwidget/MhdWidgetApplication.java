3847ead569aacd7a5a8aae51c17f35e10066955f
michal.hajek <mikrop@centrum.cz>
2018-05-19 18:59:46 +0200

    Sestavení war (production)

--- src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
+++ src/main/java/cz/mikropsoft/mhdwidget/MhdWidgetApplication.java
@@ -2,6 +2,8 @@ package cz.mikropsoft.mhdwidget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.ProjectionFactory;
@@ -11,7 +13,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 @SpringBootApplication
-public class MhdWidgetApplication {
+public class MhdWidgetApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MhdWidgetApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MhdWidgetApplication.class, args);
